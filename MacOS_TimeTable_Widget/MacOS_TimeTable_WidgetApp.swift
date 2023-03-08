3bf5339f71103689e876dae491aeaf5e6114d63f
koa <kaze11koa@gmail.com>
2023-03-08 14:40:40 +0900

    Initial Commit

---
+++ MacOS_TimeTable_Widget/MacOS_TimeTable_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOS_TimeTable_WidgetApp.swift
+//  MacOS_TimeTable_Widget
+//
+//  Created by 서정찬 on 2023/03/08.
+//
+
+import SwiftUI
+
+@main
+struct MacOS_TimeTable_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
